3cd4b45966981d3c7b8e89292104f924546eceba
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-03-28 18:26:54 +0100

    allineamento button nav-bar

--- Progetti prova/demon-slayer-site/src/app/components/nav-bar/nav-bar.component.ts
+++ Progetti prova/demon-slayer-site/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
+
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-nav-bar',
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, MatButtonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
