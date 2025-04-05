0fb0fefb9d143ea2b3ead1005072f7ddde58f452
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-04-05 18:27:07 +0200

    inserimento sign-in background e title

--- Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/nav-bar/nav-bar.component.ts
+++ Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+  
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
