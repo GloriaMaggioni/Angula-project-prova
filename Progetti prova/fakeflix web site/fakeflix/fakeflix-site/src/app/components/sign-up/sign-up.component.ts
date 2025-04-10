baedffca1c22f795d91e9d4f7d78b050ce9a0333
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-04-10 21:03:25 +0200

    add global background

--- Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/sign-up/sign-up.component.ts
+++ Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/sign-up/sign-up.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { AppComponent } from "../../app.component";
 
 @Component({
   selector: 'app-sign-up',
-  imports: [NavBarComponent, ],
+  imports: [NavBarComponent, AppComponent],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
