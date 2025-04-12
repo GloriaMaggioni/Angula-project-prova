f71932ee9e8eb142f5eb7feb809da2d376f69a94
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-04-13 00:06:10 +0200

    add animazione input

--- Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/sign-up/sign-up.component.ts
+++ Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
-import { AppComponent } from "../../app.component";
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, Validators} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-sign-up',
-  imports: [NavBarComponent, AppComponent],
+  imports: [NavBarComponent, MatInputModule, MatFormFieldModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
