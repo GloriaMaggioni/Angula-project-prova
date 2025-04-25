484872c8e1cb911e5153e9ab979482d52e696055
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-04-25 12:01:17 +0200

    centrato i buttons

--- Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/sign-up/sign-up.component.ts
+++ Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TitlePageComponent } from '../title-page/title-page.component';
 import { FooterComponent } from "../footer/footer.component";
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
@@ -9,10 +10,18 @@ import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [TitlePageComponent, FooterComponent,MatCardModule],
+  imports: [TitlePageComponent, FooterComponent,MatCardModule, MatButtonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+   btnLeft(){
+    const btnLeft:any = document.querySelector('.btn-left') ;
+  
+      btnLeft.addEventListener('click', () =>{
+        console.log('FUNZIONAAA')
+      })
+    }
+   
 
 }
