8baf655dd955d2b940059f47bc72b68e51b5a0a0
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-05-13 23:37:41 +0200

    add social icons in the footer

--- Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/app.component.ts
+++ Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LoginComponent } from './components/login/login.component';
 
+
 @Component({
   selector: 'app-root',
   imports: [
--- Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/footer/footer.component.ts
+++ Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/footer/footer.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+ import{FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faSquareInstagram}from '@fortawesome/free-brands-svg-icons'
+import{faAt}from '@fortawesome/free-solid-svg-icons'
+import {faGithub}from '@fortawesome/free-brands-svg-icons';
+import {faLinkedin}from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatIconModule, 
+    FontAwesomeModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faInstagram = faSquareInstagram;
+  faAt = faAt;
+  faGithub = faGithub;
+  faLinkedin= faLinkedin;
 }
