0e49f4a882f968cc9fdaa4a47f47045abf010929
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-06-16 12:02:33 +0200

    add control form(da sistemare)

--- Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/login/login.component.ts
+++ Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/login/login.component.ts
@@ -1,30 +1,65 @@
-import { Component, ElementRef, input, ViewChild, } from '@angular/core';
+import { Component, ElementRef, input, ViewChild, signal, HostListener, Renderer2} from '@angular/core';
 import {  Router, RouterModule } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
 import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
-import { NgIf } from '@angular/common';
+import { NgIf, NgStyle } from '@angular/common';
+import {merge} from 'rxjs';
+import { error } from 'node:console';
 
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule, MatInputModule,  FormsModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule],
+  imports: [RouterModule, MatInputModule,  FormsModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule,NgStyle ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+
 })
 export class LoginComponent {
- 
-  constructor(private route: Router){}
+  @ViewChild('email') emailInput! : ElementRef <HTMLInputElement>
+  @ViewChild('password') passwordInput! : ElementRef <HTMLInputElement>
+  constructor(private route: Router, public renderer : Renderer2){}
+
+
   
+  myForm: FormGroup = new FormGroup ({
+    email : new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  })
+ 
+  errorStyle(){
+    const email = this.emailInput.nativeElement;
+    const password = this.passwordInput.nativeElement;
+    
+    if(this.myForm.invalid && this.myForm.touched){
+      this.renderer.setStyle(email, 'border', '1px solid red')
+       this.renderer.setStyle(password, 'border', '1px solid red')
+    }else{
+       this.renderer.setStyle(email, 'border', '1px solid gray')
+       this.renderer.setStyle(password, 'border', '1px solid gray')
+
+    }
+  }
+
+
+
+
+
+
+
+
 
 
   goToSignUpPage(){
-    this.route.navigate(['sign-up'])
-  }
+     this.route.navigate(['sign-up'])
+   }
 
- 
- 
- 
+   backToSignUpPage(){
+     this.route.navigate(['sign-up'])
+   }
 
+
+
+  
 }
