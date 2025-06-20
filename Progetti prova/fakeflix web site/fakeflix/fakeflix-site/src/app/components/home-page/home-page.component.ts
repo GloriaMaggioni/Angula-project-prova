36f3f6ed072e2564d8be8d8ff274980b60640328
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-06-20 16:46:36 +0200

    add banner's background

--- Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/home-page/home-page.component.ts
+++ Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatSelectModule, MatFormFieldModule, RouterModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+  constructor(private route: Router){}
+
+
+  goBackToSignUpPage(){
+     this.route.navigate(['sign-up'])
+
+  }
 
 }
--- Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/login/login.component.ts
+++ Progetti prova/fakeflix web site/fakeflix/fakeflix-site/src/app/components/login/login.component.ts
@@ -1,12 +1,11 @@
-import { Component, ElementRef, input, ViewChild, signal, HostListener, Renderer2} from '@angular/core';
+import { Component, } from '@angular/core';
 import {  Router, RouterModule } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import { NgIf, NgStyle } from '@angular/common';
-import {merge} from 'rxjs';
-import { error } from 'node:console';
+
 
 
 @Component({
